b12e25f0f74fab9023efcdb6d47ca31ed3ec6f82
Arnaldo Araujo <arnaldoaraujo@unitins.br>
2024-09-30 11:18:29 -0300

    2024-09-30-Alteração no exemplo de alteração.

--- TrabalhoAula008/src/App.java
+++ TrabalhoAula008/src/App.java
@@ -11,18 +11,19 @@ public class App {
         gen.CadastrarPessoa(pessoa1);
 
         Pessoa pessoa2 = new Pessoa();
-        pessoa2.setNome("Ezequiel M de Araujo");
+        pessoa2.setNome("Fulano M de Araujo");
         pessoa2.setCpf("124.235.687-98");
         pessoa2.setIdade(18);
         gen.CadastrarPessoa(pessoa2);
         gen.exibirPessoa();
 
         System.out.println("Atualizar nome pessoa 2");
-        pessoa2.setNome("Ezequiel Mendonça de Araujo");
+        pessoa2.setNome("Fulano Moraes de Araujo");
         gen.atualizarPessoa(pessoa2);
         gen.exibirPessoa();
 
         System.out.println("Atualizar cpf pessoa 1");
+        
         pessoa1.setCpf("191.365.558-22");
 
         gen.atualizarPessoa(pessoa1);
